f1cbf3400890051bb3152862c629e80776d09f04
Denifia <mr.l.wale@gmail.com>
2025-04-08 08:32:12 +0800

    trying to sort out keymaps
    Added hawtkeys to help detect duplicate keymaps and suggest new
    ones. Mapped git status changes to '<leader>sc'.

---
+++ lua/plugins/hawtkeys.lua
@@ -0,0 +1,23 @@
+return {
+  'tris203/hawtkeys.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = {
+    -- an empty table will work for default config
+    --- if you use functions, or whichkey, or lazy to map keys
+    --- then please see the API below for options
+    keyboardLayout = 'colemak',
+    customMaps = {
+      -- If you use whichkey.register with an alias eg wk.register
+      ['wk.register'] = {
+        method = 'which_key',
+      },
+      -- If you use lazy.nvim's keys property to configure keymaps in your plugins
+      ['lazy'] = {
+        method = 'lazy',
+      },
+    },
+  },
+}
