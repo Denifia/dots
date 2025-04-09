cc0e1640921673ec064c8afeedefa85a8766109c
Denifia <mr.l.wale@gmail.com>
2025-04-09 11:31:04 +0800

    added project.nvim
    Auto detects project root directory using a variety of techniques.
    Gets it right most of the time.

---
+++ lua/plugins/project.lua
@@ -0,0 +1,8 @@
+return {
+  -- https://github.com/ahmedkhalf/project.nvim
+  -- Auto detects project root for use with file explorer (Oil)
+  'ahmedkhalf/project.nvim',
+  config = function()
+    require('project_nvim').setup {}
+  end,
+}
